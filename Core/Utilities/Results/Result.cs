5544361260460810db1e4a91de22f1779b7d1712
akın ayhan <akinayhan@hotmail.com.tr>
2021-02-13 22:59:01 +0300

    aa

--- Core/Utilities/Results/Result.cs
+++ Core/Utilities/Results/Result.cs
@@ -6,6 +6,16 @@ namespace Core.Utilities.Results
 {
     public class Result : IResult
     {
+        public Result(bool succes, string message):this(succes)
+        {
+            Message = message;
+        }
+
+        public Result(bool succes)
+        {
+            Success = succes;
+        }
+
         public bool Success { get; }
 
         public string Message { get; }
